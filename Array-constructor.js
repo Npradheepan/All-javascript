0ac0a18550fda0a83ff944ad01e8b09144aa9f0a
Npradheepan <44838133+Npradheepan@users.noreply.github.com>
2019-08-19 21:49:03 +0200

    Create Array-constructor.js

---
+++ Array-constructor.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Array-Constructor.js</title>
+</head>
+<script>
+    function clickme(){
+        var fruit=["Apple","Mango","banana"];
+        document.getElementById("result").innerHTML=fruit.constructor;
+    }
+</script>
+<body>
+    <button onclick="clickme()"> click</button>
+    <p id="result"></p>
+</body>
+</html>
